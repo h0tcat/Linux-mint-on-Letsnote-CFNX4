f119933f3f715a5a15bebbabcea5ed8e23d116a1
KenchanPrfm <63098775+KenchanPrfm@users.noreply.github.com>
2020-07-06 23:28:04 +0900

    Add:change pipe to tee command

--- initial_setup/LM20/initial_setup.sh
+++ initial_setup/LM20/initial_setup.sh
@@ -2,12 +2,12 @@
 ##########
 # レポジトリを日本に切り替える
 ##########
-sudo echo "deb http://ftp.jaist.ac.jp/pub/Linux/linuxmint/packages ulyana main upstream import backport" >> /etc/apt/sources.list.d/official-package-repositories.list
-sudo echo "deb http://ftp.jaist.ac.jp/pub/Linux/ubuntu focal main restricted universe multiverse" >> /etc/apt/sources.list.d/official-package-repositories.list
-sudo echo "deb http://ftp.jaist.ac.jp/pub/Linux/ubuntu focal-updates main restricted universe multiverse" >> /etc/apt/sources.list.d/official-package-repositories.list
-sudo echo "deb http://ftp.jaist.ac.jp/pub/Linux/ubuntu focal-backports main restricted universe multiverse" >> /etc/apt/sources.list.d/official-package-repositories.list
-sudo sed -i -e "/http:\/\/packages.linuxmint.com\/s/^/#/" /etc/apt/sources.list.d/official-package-repositories.list > /etc/apt/sources.list.d/official-package-repositories.list
-sudo sed -i -e "/http:\/\/archive.ubuntu.com/ubuntu\/s/^/#/g" /etc/apt/sources.list.d/official-package-repositories.list > /etc/apt/sources.list.d/official-package-repositories.list
+sudo echo "deb http://ftp.jaist.ac.jp/pub/Linux/linuxmint/packages ulyana main upstream import backport" | sudo tee -a /etc/apt/sources.list.d/official-package-repositories.list
+sudo echo "deb http://ftp.jaist.ac.jp/pub/Linux/ubuntu focal main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list.d/official-package-repositories.list
+sudo echo "deb http://ftp.jaist.ac.jp/pub/Linux/ubuntu focal-updates main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list.d/official-package-repositories.list
+sudo echo "deb http://ftp.jaist.ac.jp/pub/Linux/ubuntu focal-backports main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list.d/official-package-repositories.list
+sudo sed -i -e "/http:\/\/packages.linuxmint.com\/s/^/#/" /etc/apt/sources.list.d/official-package-repositories.list | sudo tee /etc/apt/sources.list.d/official-package-repositories.list
+sudo sed -i -e "/http:\/\/archive.ubuntu.com/ubuntu\/s/^/#/g" /etc/apt/sources.list.d/official-package-repositories.list | sudo tee /etc/apt/sources.list.d/official-package-repositories.list
 
 # ソースの更新と更新パッケージの取得
 sudo apt update && sudo apt upgrade
@@ -33,12 +33,12 @@ LANG=C xdg-user-dirs-gtk-update
 sudo apt install -y xserver-xorg-input-synaptics
 
 # xinput設定（トラックパッドの感度を下げる）
-echo "# Touchpad Speed Configuration" >> ~/.xinputrc
-echo $(xinput --set-prop $(echo $(xinput --list --short | grep -i touchpad) | sed -e 's/.\+id=\([0-9]\+\).\+$/\1/g') $(xinput ---list-props $(echo $(xinput --list --short | grep -i touchpad) | sed -e 's/.\+id=\([0-9]\+\).\+$/\1/g') | grep Constant | sed -e 's/.\+(\([0-9]\+\)).\+/\1/g') 7.5) >> ~/.xinputrc
+echo "# Touchpad Speed Configuration" | tee -a ~/.xinputrc
+echo $(xinput --set-prop $(echo $(xinput --list --short | grep -i touchpad) | sed -e 's/.\+id=\([0-9]\+\).\+$/\1/g') $(xinput ---list-props $(echo $(xinput --list --short | grep -i touchpad) | sed -e 's/.\+id=\([0-9]\+\).\+$/\1/g') | grep Constant | sed -e 's/.\+(\([0-9]\+\)).\+/\1/g') 7.5) | tee -a ~/.xinputrc
 
 # Synaptics Configuration activete CircularScrolling
 cp /usr/share/X11/xorg.conf.d/51-synaptics-quirks.conf ~/51-synaptics-quirks.conf.bk
-sudo echo "Driver \"synaptics\"\n        Option \"LeftEdge\" \"1000\"\n        Option \"RightEdge\" \"5600\"\n        Option \"TopEdge\" \"1200\"\n        Option \"BottomEdge\" \"4800\"\n        MatchIsTouchPad \"on\"\n        Option \"CircularScrolling\" \"1\"\n        Option \"CircScrollTrigger\" \"0\"\n        Option \"CircularPad\" \"1\"\n        Option \"CoastingSpeed\" \"1\"\n        Option \"CoastingFunction\" \"40\"\nEndSection" /usr/share/X11/xorg.conf.d/51-synaptics-quirks.conf > /usr/share/X11/xorg.conf.d/51-synaptics-quirks.conf
+sudo echo -e "Section \"InputClass\"\n                Identifier \"touchpad catchall\"\n        Driver \"synaptics\"\n        Option \"LeftEdge\" \"1000\"\n        Option \"RightEdge\" \"5600\"\n        Option \"TopEdge\" \"1200\"\n        Option \"BottomEdge\" \"4800\"\n        MatchIsTouchPad \"on\"\n        Option \"CircularScrolling\" \"1\"\n        Option \"CircScrollTrigger\" \"0\"\n        Option \"CircularPad\" \"1\"\n        Option \"CoastingSpeed\" \"1\"\n        Option \"CoastingFunction\" \"40\"\nEndSection" /usr/share/X11/xorg.conf.d/51-synaptics-quirks.conf | sudo tee -a /usr/share/X11/xorg.conf.d/51-synaptics-quirks.conf
 
 ##########
 # その他HW設定
@@ -46,18 +46,18 @@ sudo echo "Driver \"synaptics\"\n        Option \"LeftEdge\" \"1000\"\n        O
 # バックライトの輝度調整を有効化
 cp /etc/default/grub ~/grub.bk
 sudo su
-sed -i -e '/GRUB_CMDLINE_LINUX_DEFAULT=/s/".*"/"quiet splash acpi_backlight=video acpi_osi=!!"/g' /etc/default/grub > /etc/default/grub
+sed -i -e '/GRUB_CMDLINE_LINUX_DEFAULT=/s/".*"/"quiet splash acpi_backlight=video acpi_osi=!!"/g' /etc/default/grub | tee /etc/default/grub
 #echo 'GRUB_CMDLINE_LINUX_DEFAULT="quiet splash acpi_backlight=video acpi_osi=!!"' >> /etc/default/grub
 update-grub
 
 # サウンド設定の修正
 cp /usr/share/pulseaudio/alsa-mixer/paths/analog-output-speaker.conf ~/analog-output-speaker.conf.bk
-sed -i -e "/\[Element Headphone\]/{N;N;s/volume = off/volume = merge\noverride-map.1 = all\noverride-map.2 = all-left,all-right/g}" /usr/share/pulseaudio/alsa-mixer/paths/analog-output-speaker.conf > /usr/share/pulseaudio/alsa-mixer/paths/analog-output-speaker.conf
-sed -i -e "/\[Element Speaker\]/{N;N;N;s/volume = merge/volume = off/g}" /usr/share/pulseaudio/alsa-mixer/paths/analog-output-speaker.conf > /usr/share/pulseaudio/alsa-mixer/paths/analog-output-speaker.conf
+sed -i -e "/\[Element Headphone\]/{N;N;s/volume = off/volume = merge\noverride-map.1 = all\noverride-map.2 = all-left,all-right/g}" /usr/share/pulseaudio/alsa-mixer/paths/analog-output-speaker.conf | tee /usr/share/pulseaudio/alsa-mixer/paths/analog-output-speaker.conf
+sed -i -e "/\[Element Speaker\]/{N;N;N;s/volume = merge/volume = off/g}" /usr/share/pulseaudio/alsa-mixer/paths/analog-output-speaker.conf | tee /usr/share/pulseaudio/alsa-mixer/paths/analog-output-speaker.conf
 sudo pulseaudio -k && pulseaudio --start
 
 # pulseaudioの有効化（Bluetooth）
-sudo echo "[General]\nDisable=Socket\nEnable=Media,Source,Sink,Gateway" >> /etc/bluetooth/audio.conf
+sudo echo -e "[General]\nDisable=Socket\nEnable=Media,Source,Sink,Gateway" | sudo tee -a /etc/bluetooth/audio.conf
 sudo service bluetooth restart
 sudo pactl load-module module-bluetooth-discover
 sudo pactl load-module module-switch-on-connect
@@ -67,7 +67,7 @@ sudo pactl load-module module-switch-on-connect
 ##########
 # vimのvi互換モードをOFF
 touch ~/.vimrc
-echo "set nocompatible\nset backspace=indent,eol,start" >> ~/.vimrc
+echo -e "set nocompatible\nset backspace=indent,eol,start" | tee -a ~/.vimrc
 sudo cp ~/.vimrc /root
 
 # HW時間を変更
@@ -76,9 +76,9 @@ sudo timedatectl set-local-rtc true
 
 # NTP
 cp /etc/systemd/timesyncd.conf ~/timesyncd.conf.bk
-sed -i -e "/^#\(Fallback\)\?NTP=/s/^#//" /etc/systemd/timesyncd.conf > /etc/systemd/timesyncd.conf
-sed -i -e "/^NTP=/s/$/ntp.nict.jp/" /etc/systemd/timesyncd.conf > /etc/systemd/timesyncd.conf
-sed -i -e "/^FallbackNTP=/s/=.*$/ntp1.jst.mfeed.ad.jp ntp2.jst.mfeed.ad.jp ntp3.jst.mfeed.ad.jp/" /etc/systemd/timesyncd.conf > /etc/systemd/timesyncd.conf
+sed -i -e "/^#\(Fallback\)\?NTP=/s/^#//" /etc/systemd/timesyncd.conf | sudo tee /etc/systemd/timesyncd.conf
+sed -i -e "/^NTP=/s/$/ntp.nict.jp/" /etc/systemd/timesyncd.conf | sudo tee /etc/systemd/timesyncd.conf
+sed -i -e "/^FallbackNTP=/s/=.*$/ntp1.jst.mfeed.ad.jp ntp2.jst.mfeed.ad.jp ntp3.jst.mfeed.ad.jp/" /etc/systemd/timesyncd.conf | sudo tee /etc/systemd/timesyncd.conf
 
 ##########
 # アプリのインストール
