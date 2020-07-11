5d694d294f03d7be9e7cb58ef3f85f8d96151f75
KenchanPrfm <63098775+KenchanPrfm@users.noreply.github.com>
2020-07-11 13:40:37 +0900

    Add GoogleDrive

--- initial_setup/LM19.3/initial_setup_19.3.sh
+++ initial_setup/LM19.3/initial_setup_19.3.sh
@@ -87,6 +87,11 @@ cd ~/Downloads
 # Google Chrome
 sudo echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee -a /etc/apt/sources.list.d/google-chrome.list && wget https://dl.google.com/linux/linux_signing_key.pub && sudo apt-key add linux_signing_key.pub && sudo apt update && sudo apt install google-chrome-stable
 
+# Google Drive
+sudo add-apt-repository ppa:alessandro-strada/ppa
+sudo apt-get update
+sudo apt-get install -y google-drive-ocamlfuse
+
 # preload
 sudo apt install -y preload
 sudo /etc/init.d/preload start
